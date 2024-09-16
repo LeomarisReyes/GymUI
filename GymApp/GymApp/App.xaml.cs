102993a040383e7f366f5606ec5c3fd31e812029
Leomaris Reyes <reyes.leomaris@gmail.com>
2024-09-16 12:49:01 -0400

    Replicating Gym subscription UI in .NET MAUI

---
+++ GymApp/GymApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace GymApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ GymApp/GymApp/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace GymApp
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            
+        }
+    }
+
+}
